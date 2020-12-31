80353d143a4454065c82ec23d5007181a95165fa
Arina Gavrilenko <arina.gavrilenko@outlook.com>
2021-01-01 05:17:18 +0700

    Задание 8
    Задание 8

---
+++ Задание 8.js
@@ -0,0 +1,8 @@
+let heroes = new Map([["Imp", "Inferno"], 
+["Griffin", "Haven"], 
+["Hydra", "Dungeon"], 
+["Lich", "Necropolis"]
+]);
+for(let elem of heroes){
+	console.log(`Ключ - ${elem[0]}, значение - ${elem[1]}`);
+}
