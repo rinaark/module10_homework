3d4329d8f0c3f75b617870d72c203351625f4bf2
Arina Gavrilenko <arina.gavrilenko@outlook.com>
2020-12-31 20:58:29 +0700

    Задание 4
    Модуль 10.4, задание 4

---
+++ Задание 4.js
@@ -0,0 +1,5 @@
+let randNum = Math.floor(Math.random() * (101));
+console.log(randNum);
+
+//Math.floor - округление вниз
+//* (max - min + 1)) + min формула для диапазона (включая границы), в коде уже подсчитано
