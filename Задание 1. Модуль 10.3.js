fa5c5eede27ffa4c4e328b30d5830d2a7e20c81b
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2021-01-04 21:04:45 +0300

    Проверено

--- Задание 1. Модуль 10.3.js
+++ Задание 1. Модуль 10.3.js
@@ -1,8 +1,9 @@
 
-let numToCheck = prompt ("Введите значение");
-numToCheck++;
-numToCheck--;
-if ((typeof numToCheck === "number") && (Number.isNaN(numToCheck) === false))
+let numToCheck = +prompt ("Введите значение");
+//numToCheck++;
+//numToCheck--;
+// Вместо прибавления и отнимания от числа 1 лучше использовать унарный плюс (дописала его выше)
+if (typeof numToCheck === "number" && !Number.isNaN(numToCheck)) // Вместо сравнивания значения с false можно просто воспользоваться логическим отрицанием (знак !), так код будет компактнее. Скобки между двумя частями логического оператора в данном случае также лишние.
 {
 	if (numToCheck % 2 === 0)
 	{
